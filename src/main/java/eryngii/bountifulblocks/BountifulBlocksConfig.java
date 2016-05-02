f252756a5790f2f8b8a048791470e45d084df6ed
Hayabusa <yuuta.keiko.k@live.jp>
2016-05-03 06:34:41 +0900

    Add config class. -add コンフィグファイルの生成と読み込みを担うクラスを追加 -modify メインクラスのコンフィグ処理のコメントアウトを解除

---
+++ src/main/java/eryngii/bountifulblocks/BountifulBlocksConfig.java
@@ -0,0 +1,26 @@
+package eryngii.bountifulblocks;
+
+import java.io.File;
+
+import net.minecraftforge.common.config.Configuration;
+
+public class BountifulBlocksConfig {
+	 //モード判定用変数
+	static boolean is1_8Mode;
+	static boolean is1_9Mode;
+
+	public static void preLoad(File file){
+		Configuration cfg = new Configuration(file);
+
+		try {
+			System.out.println("生成");
+			//それぞれの項目追加
+			is1_8Mode = cfg.getBoolean("1.8Mode", "General", true, "Whether to add an element of 1.8.");
+			is1_9Mode = cfg.getBoolean("1.9mode", "General", true, "Whether to add an element of 1.9.");
+			cfg.load();
+		}finally{
+			cfg.save();
+			System.out.println("セーブ");
+		}
+	}
+}
--- src/main/java/eryngii/bountifulblocks/BountifulBlocksCore.java
+++ src/main/java/eryngii/bountifulblocks/BountifulBlocksCore.java
@@ -1,5 +1,7 @@
 package eryngii.bountifulblocks;
 
+import java.io.File;
+
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
@@ -25,10 +27,10 @@ public class BountifulBlocksCore {
 	 * メタデータ関連の処理は赤砂蛇凪浜様のWikiと、
 	 * reginn666氏のWikiを参考にさせていただきました。
 	 * */
-	
+
 	  //全面同じテクスチャのブロック。代表者はDiorite
 	  public static Block blockDiorite;
-	  //土と同じ耐久レベルのブロック 
+	  //土と同じ耐久レベルのブロック
 	  public static Block blockCoarseDirt;
 	  //フェンス類。
 	  public static Block blockFenceX;
@@ -41,10 +43,10 @@ public class BountifulBlocksCore {
 	  public static Block blockSmoothRedSandStone;
 	  public static Block blockChiseledRedSandStone;
 	  public static Block blockPurpurPillar;
-	  
+
 	  //明かり類。今回はシーランタンのみ
 	  public static Block blockSeaLantern;
-	  
+
 	  //以下MayaCraft
 	  public static Block blockCannabis;//苗
 	  public static Item itemCannabisPlant;//全体
@@ -56,13 +58,12 @@ public class BountifulBlocksCore {
 	  public void preInit(FMLPreInitializationEvent event)
 	{
 			//コンフィグをmodidではなく任意の名前で生成するためにFileインスタンスを生成してコンフィグクラスに渡している
-			/*
 		  File cfgfile = new File(event.getModConfigurationDirectory(), "BountifulBlocksMod.cfg");
 			BountifulBlocksConfig.preLoad(cfgfile);
-			*/
+
 
 			/*メタデータを保持するブロック類。registerBlockメソッドの第二引数に該当クラスを渡している*/
-			
+
 			blockDiorite = new DioriteBlock();
 			GameRegistry.registerBlock(blockDiorite, ItemDioriteBlock.class, "blockDiorite");
 
@@ -72,14 +73,14 @@ public class BountifulBlocksCore {
 			//ここで指定するテクスチャは、Minecraft本体のテクスチャファイル内を探していて都合が悪いので空
 			blockFenceX = new FenceBlockX("", Material.wood);
 			GameRegistry.registerBlock(blockFenceX, ItemFenceBlockX.class, "blockFenceX");
-			
+
 			//blockHalfX = new HalfBlockX(null,0);
 			//GameRegistry.registerBlock(blockHalfX, ItemHalfBlockX.class, "blockHalfX");
 			/*
 			blockStairX = new DioriteBlock();
 			GameRegistry.registerBlock(blockDiorite, ItemDioriteBlock.class, "blockDiorite");
 			*/
-		
+
 			/*以下メタデータを保持しないブロック類*/
 
 
@@ -88,52 +89,52 @@ public class BountifulBlocksCore {
 
 			blockChiseledRedSandStone = new CRSSBlock();
 			GameRegistry.registerBlock(blockChiseledRedSandStone, "blockCRSS");
-			
+
 			blockSmoothRedSandStone = new SRSSBlock();
 			GameRegistry.registerBlock(blockSmoothRedSandStone, "blockSRSS");
 
 			blockPurpurPillar = new PurpurPillarBlock();
 			GameRegistry.registerBlock(blockPurpurPillar, "blockPurpurPillar");
-			
+
 			blockBarrier = new BarrierBlock();
 			GameRegistry.registerBlock(blockBarrier, "blockBarrier");
-			
+
 			blockSeaLantern = new SeaLanternBlock();
 			GameRegistry.registerBlock(blockSeaLantern, "blockSeaLantern");
-			
+
 			blockCannabis = new CannabisBlock();
 			GameRegistry.registerBlock(blockCannabis, "blockCannabis");
-			
+
 			itemCannabisPlant = new Item()
 					.setCreativeTab(CreativeTabs.tabMaterials)/*クリエイティブのタブ*/
 					.setUnlocalizedName("itemCannabisPlant")/*システム名の登録*/
 					.setTextureName("bountifulmod:cannabis_plant");/*テクスチャの指定*/
 			GameRegistry.registerItem(itemCannabisPlant, "itemCannabisPlant");
-			
+
 			itemCannabisLeaf = (new ItemCannabisLeaf(0, 0.0F, false))
 					.setCreativeTab(CreativeTabs.tabFood)/*クリエイティブのタブ*/
 					.setUnlocalizedName("itemCannabisLeaf")/*システム名の登録*/
 					.setTextureName("bountifulmod:cannabis_leaf");/*テクスチャの指定*/
 			GameRegistry.registerItem(itemCannabisLeaf, "itemCannabisLeaf");
-			
+
 			itemCannabisPowder = new Item()
 					.setCreativeTab(CreativeTabs.tabFood)/*クリエイティブのタブ*/
 					.setUnlocalizedName("itemCannabisPowder")/*システム名の登録*/
 					.setTextureName("bountifulmod:cannabis_powder");/*テクスチャの指定*/
 			GameRegistry.registerItem(itemCannabisPowder, "itemCannabisPowder");
-			
-			
 
 
 
 
-		
-			
+
+
+
+
 }
 
 @Mod.EventHandler
 public void init(FMLInitializationEvent event){
-	
+
 	//以下レシピ登録
     GameRegistry.addRecipe(new ItemStack(blockDiorite,2,0),
             "#@",
@@ -146,55 +147,55 @@ public void init(FMLInitializationEvent event){
     	       new ItemStack(blockDiorite,1,0),
     	       Blocks.cobblestone
     	);
-    
+
     GameRegistry.addShapelessRecipe(new ItemStack(blockDiorite,2,2),
     		new ItemStack(blockDiorite,1,0),
  	       Items.quartz
  	);
-	
+
     GameRegistry.addRecipe(new ItemStack(blockDiorite,4,3),
             "##",
             "##",
             '#', new ItemStack(blockDiorite,1,0)
     );
-    
+
     GameRegistry.addRecipe(new ItemStack(blockDiorite,4,4),
             "##",
             "##",
             '#', new ItemStack(blockDiorite,1,1)
     );
-    
+
     GameRegistry.addRecipe(new ItemStack(blockDiorite,4,5),
             "##",
             "##",
             '#', new ItemStack(blockDiorite,1,2)
     );
-    
+
     GameRegistry.addRecipe(new ItemStack(blockCoarseDirt,4,0),
             "#@",
             "@#",
             '#', Blocks.gravel,
             '@', Blocks.dirt
     );
-    
+
     GameRegistry.addRecipe(new ItemStack(blockDiorite,4,8),
             "#@",
             "@#",
             '#', new ItemStack(Items.dye,1,4),
             '@', Blocks.cobblestone
     );
-    
-    
+
+
     GameRegistry.addShapelessRecipe(new ItemStack(blockDiorite,1,9),
     		new ItemStack(blockDiorite,1,8),
  	        new ItemStack(Items.dye,1,2)
  	);
-    
+
     GameRegistry.addShapelessRecipe(new ItemStack(blockDiorite,1,10),
     		new ItemStack(blockDiorite,1,8),
  	        new ItemStack(Items.dye,1,1)
  	);
-    
+
     GameRegistry.addShapelessRecipe(new ItemStack(blockDiorite,1,11),
     		new ItemStack(blockDiorite,1,8),
  	        new ItemStack(Items.dye,1,4)
@@ -205,7 +206,7 @@ public void init(FMLInitializationEvent event){
             "##",
             '#', new ItemStack(blockDiorite,1,8)
     );
-    
+
     GameRegistry.addRecipe(new ItemStack(blockDiorite,4,7),
             "###",
             "#@#",
@@ -220,51 +221,51 @@ public void init(FMLInitializationEvent event){
             '#', new ItemStack(blockDiorite,1,8) ,
             '@', Blocks.redstone_lamp
     );
-    
-    
+
+
     GameRegistry.addRecipe(new ItemStack(blockRedSandStone),
             "##",
             "##",
             '#', new ItemStack(Blocks.sand,1,1)
     );
-    
+
     GameRegistry.addRecipe(new ItemStack(blockSmoothRedSandStone),
             "##",
             "##",
             '#', new ItemStack(blockRedSandStone)
     );
-    
+
     GameRegistry.addRecipe(new ItemStack(Blocks.stonebrick,1,3),
             "#",
             "#",
             '#', new ItemStack(Blocks.stone_slab,1,5)
     );
-    
+
     GameRegistry.addShapelessRecipe(new ItemStack(Blocks.mossy_cobblestone),
     		Blocks.cobblestone,
  	        new ItemStack(Blocks.vine)
  	);
-    
+
     GameRegistry.addShapelessRecipe(new ItemStack(Blocks.stonebrick,1,1),
     		new ItemStack(Blocks.stonebrick,1,0),
  	        new ItemStack(Blocks.vine)
  	);
-    
 
-    
+
+
     GameRegistry.addRecipe(new ItemStack(blockDiorite,1,13),
             "##",
             "##",
             '#', Blocks.end_stone
     );
-    
+
     GameRegistry.addRecipe(new ItemStack(blockDiorite,1,12),
             "#@",
             "@#",
             '#', Blocks.end_stone,
             '@', new ItemStack(Items.dye,1,5)
       );
-    
+
     GameRegistry.addRecipe(new ItemStack(blockBarrier),
             "###",
             "# #",
@@ -272,27 +273,27 @@ public void init(FMLInitializationEvent event){
             '#', Blocks.glass
             );
 
-	  
-	 
+
+
 	     GameRegistry.addSmelting(new ItemStack(Blocks.stonebrick,1,0),new ItemStack(Blocks.stonebrick,1,2),0.1f);
-	 
-	     
+
+
 	     GameRegistry.addShapelessRecipe(new ItemStack(itemCannabisLeaf,4),
                   new ItemStack(itemCannabisPlant,1)
 	  	);
-	     
+
 	     GameRegistry.addRecipe(new ItemStack(blockCannabis,3),
                 "@@",
                 "@@",
 	    		 '@', new ItemStack(itemCannabisPlant,1)
 	  	);
-	     
+
 	     GameRegistry.addShapelessRecipe(new ItemStack(itemCannabisPowder,2),
                  new ItemStack(itemCannabisLeaf,1)
 	  	);
 	     }
-	        
-	    
+
+
 
 }
 
