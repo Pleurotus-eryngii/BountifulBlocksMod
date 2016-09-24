e18fe541e66a609612baa1609cadaf47c14b1d2c
Pleurotus-eryngii <imkaukmo@gmail.com>
2016-09-24 14:42:22 +0900

    add config system
    -add 1.8/1.9/1.10 switch system

--- src/main/java/eryngii/bountifulblocks/BountifulBlocksConfig.java
+++ src/main/java/eryngii/bountifulblocks/BountifulBlocksConfig.java
@@ -8,7 +8,7 @@ public class BountifulBlocksConfig {
 	 //モード判定用変数
 	static boolean is1_8Mode;
 	static boolean is1_9Mode;
-	 public static boolean achievement = true;
+	static boolean is1_10Mode;
 	 
 	 public static int professionIdMayaMer = 1211;
 
@@ -20,6 +20,7 @@ public class BountifulBlocksConfig {
 			//それぞれの項目追加
 			is1_8Mode = cfg.getBoolean("1.8Mode", "General", true, "Whether to add an element of 1.8.");
 			is1_9Mode = cfg.getBoolean("1.9mode", "General", true, "Whether to add an element of 1.9.");
+			is1_10Mode = cfg.getBoolean("1.10mode", "General", true, "Whether to add an element of 1.10.");
 			cfg.load();
 		}finally{
 			cfg.save();
@@ -28,10 +29,5 @@ public class BountifulBlocksConfig {
 
 		}
 	}
-	/*
-	  public static void load(Configuration cfgfile)
-	    {
-		  achievement = cfgfile.get("general", "achievement", achievement).getBoolean(true);
-	    }
-*/
+
 }
--- src/main/java/eryngii/bountifulblocks/RecipeRegister.java
+++ src/main/java/eryngii/bountifulblocks/RecipeRegister.java
@@ -9,6 +9,14 @@ public class RecipeRegister {
 	public static void init(){
 		
 		//以下レシピ登録
+	   if(BountifulBlocksConfig.is1_8Mode){
+		GameRegistry.addRecipe(new ItemStack(BountifulBlocksCore.blockCoarseDirt,4,0),
+	            "#@",
+	            "@#",
+	            '#', Blocks.gravel,
+	            '@', Blocks.dirt
+	    );
+		
 	    GameRegistry.addRecipe(new ItemStack(BountifulBlocksCore.blockDiorite,2,0),
 	            "#@",
 	            "@#",
@@ -44,13 +52,6 @@ public class RecipeRegister {
 	            '#', new ItemStack(BountifulBlocksCore.blockDiorite,1,2)
 	    );
 	    
-	    GameRegistry.addRecipe(new ItemStack(BountifulBlocksCore.blockCoarseDirt,4,0),
-	            "#@",
-	            "@#",
-	            '#', Blocks.gravel,
-	            '@', Blocks.dirt
-	    );
-	    
 	    GameRegistry.addRecipe(new ItemStack(BountifulBlocksCore.blockDiorite,4,8),
 	            "#@",
 	            "@#",
@@ -58,6 +59,19 @@ public class RecipeRegister {
 	            '@', Blocks.cobblestone
 	    );
 	    
+	    GameRegistry.addRecipe(new ItemStack(BountifulBlocksCore.blockDiorite,4,6),
+	            "##",
+	            "##",
+	            '#', new ItemStack(BountifulBlocksCore.blockDiorite,1,8)
+	    );
+	    
+	    GameRegistry.addRecipe(new ItemStack(BountifulBlocksCore.blockDiorite,4,7),
+	            "###",
+	            "#@#",
+	            "###",
+	            '#', new ItemStack(BountifulBlocksCore.blockDiorite,1,8),
+	            '@', new ItemStack(Items.dye,1,0)
+	    );
 	    
 	    GameRegistry.addShapelessRecipe(new ItemStack(BountifulBlocksCore.blockDiorite,1,9),
 	    		new ItemStack(BountifulBlocksCore.blockDiorite,1,8),
@@ -73,7 +87,130 @@ public class RecipeRegister {
 	    		new ItemStack(BountifulBlocksCore.blockDiorite,1,8),
 	 	        new ItemStack(Items.dye,1,4)
 	 	);
+	    
+	    GameRegistry.addRecipe(new ItemStack(BountifulBlocksCore.blockSeaLantern),
+	            "###",
+	            "#@#",
+	            "###",
+	            '#', new ItemStack(BountifulBlocksCore.blockDiorite,1,8) ,
+	            '@', Blocks.redstone_lamp
+	    );
+	    
+	    GameRegistry.addRecipe(new ItemStack(BountifulBlocksCore.blockRedSandStone),
+	            "##",
+	            "##",
+	            '#', new ItemStack(Blocks.sand,1,1)
+	    );
+	    
+	    GameRegistry.addRecipe(new ItemStack(BountifulBlocksCore.blockSmoothRedSandStone),
+	            "##",
+	            "##",
+	            '#', new ItemStack(BountifulBlocksCore.blockRedSandStone)
+	    );
+	    
+	    GameRegistry.addRecipe(new ItemStack(BountifulBlocksCore.blockChiseledRedSandStone,3),
+	            "###",
+	            "   ",
+	            "###",
+	            '#', new ItemStack(BountifulBlocksCore.blockRedSandStone)
+	   
+	    );
+	    
+	    GameRegistry.addRecipe(new ItemStack(BountifulBlocksCore.blockFenceX,4,0),
+	            "   ",
+	    		"@@@",
+	            "@ @",
+	            '@', new ItemStack(Blocks.planks,1,4)
+	      );
+	      
+	    GameRegistry.addRecipe(new ItemStack(BountifulBlocksCore.blockFenceX,4,1),
+	            "   ",
+	    		"@@@",
+	            "@ @",
+	            '@', new ItemStack(Blocks.planks,1,5)
+	      );
+	    
+	    GameRegistry.addRecipe(new ItemStack(BountifulBlocksCore.blockFenceX,4,2),
+	    		"   ",
+	    		"@@@",
+	            "@ @",
+	            '@', new ItemStack(Blocks.planks,1,2)
+	      );
+	    
+	    GameRegistry.addRecipe(new ItemStack(BountifulBlocksCore.blockFenceX,4,3),
+	    		"   ",
+	    		"@@@",
+	            "@ @",
+	            '@', new ItemStack(Blocks.planks,1,1)
+	      );
+	    
+	    GameRegistry.addRecipe(new ItemStack(BountifulBlocksCore.blockFenceX,4,4),
+	    		"   ",
+	    		"@@@",
+	            "@ @",
+	            '@', new ItemStack(Blocks.planks,1,3)
+	      );
+	    
+	    GameRegistry.addRecipe(new ItemStack(Blocks.stonebrick,1,3),
+	            "#",
+	            "#",
+	            '#', new ItemStack(Blocks.stone_slab,1,5)
+	    );
+	    
+	    GameRegistry.addShapelessRecipe(new ItemStack(Blocks.mossy_cobblestone),
+	    		Blocks.cobblestone,
+	 	        new ItemStack(Blocks.vine)
+	 	);
+	    
+	    GameRegistry.addShapelessRecipe(new ItemStack(Blocks.stonebrick,1,1),
+	    		new ItemStack(Blocks.stonebrick,1,0),
+	 	        new ItemStack(Blocks.vine)
+	 	);
+	    
+	    GameRegistry.addRecipe(new ItemStack(BountifulBlocksCore.blockBarrier),
+	            "###",
+	            "# #",
+	            "###",
+	            '#', Blocks.glass
+	            );
+	    
+		GameRegistry.addSmelting(new ItemStack(Blocks.stonebrick,1,0),new ItemStack(Blocks.stonebrick,1,2),0.1f);
+		
+		GameRegistry.addSmelting(new ItemStack(BountifulBlocksCore.rawRabbit,1),new ItemStack(BountifulBlocksCore.cookedRabbit,1),0.35f);
+	    
+	    GameRegistry.addRecipe(new ItemStack(Items.leather),
+	            "##",
+	            "##",
+	            '#', BountifulBlocksCore.rabbitHide
+	    );
+	   }
+	    
+	    if(BountifulBlocksConfig.is1_9Mode)	{    
+	    GameRegistry.addRecipe(new ItemStack(BountifulBlocksCore.blockDiorite,1,12),
+	            "#@",
+	            "@#",
+	            '#', Blocks.end_stone,
+	            '@', new ItemStack(Items.dye,1,5)
+	      );
+	    
+	    GameRegistry.addRecipe(new ItemStack(BountifulBlocksCore.blockDiorite,1,13),
+	            "##",
+	            "##",
+	            '#', Blocks.end_stone
+	    );
+	        
+	    GameRegistry.addRecipe(new ItemStack(BountifulBlocksCore.blockPurpurPillar),
+	            "@",
+	            "@",
+	            '@', new ItemStack(BountifulBlocksCore.blockDiorite,1,12)
+	      );
+	      
+	    GameRegistry.addShapelessRecipe(new ItemStack(BountifulBlocksCore.blockCoarseDirt,3,1),
+	    		new ItemStack(Blocks.dirt,9)
+	 	);
+	    }
 
+	    if(BountifulBlocksConfig.is1_10Mode){
 	    GameRegistry.addRecipe(new ItemStack(BountifulBlocksCore.blockDiorite,1,14),
 	            "###",
 	            "###",
@@ -99,145 +236,15 @@ public class RecipeRegister {
 	   
 	    );
 	    
-	    GameRegistry.addRecipe(new ItemStack(BountifulBlocksCore.blockDiorite,4,6),
-	            "##",
-	            "##",
-	            '#', new ItemStack(BountifulBlocksCore.blockDiorite,1,8)
-	    );
-	    
-	    GameRegistry.addRecipe(new ItemStack(BountifulBlocksCore.blockDiorite,4,7),
-	            "###",
-	            "#@#",
-	            "###",
-	            '#', new ItemStack(BountifulBlocksCore.blockDiorite,1,8),
-	            '@', new ItemStack(Items.dye,1,0)
-	    );
-	    GameRegistry.addRecipe(new ItemStack(BountifulBlocksCore.blockSeaLantern),
-	            "###",
-	            "#@#",
-	            "###",
-	            '#', new ItemStack(BountifulBlocksCore.blockDiorite,1,8) ,
-	            '@', Blocks.redstone_lamp
-	    );
-	   
 	    GameRegistry.addRecipe(new ItemStack(BountifulBlocksCore.blockMagma),
 	            "##",
 	            "##",
 	            '#', new ItemStack(Items.magma_cream) 
 	    );
+	    }
 	    
-	    GameRegistry.addRecipe(new ItemStack(BountifulBlocksCore.blockRedSandStone),
-	            "##",
-	            "##",
-	            '#', new ItemStack(Blocks.sand,1,1)
-	    );
-	    
-	    GameRegistry.addRecipe(new ItemStack(BountifulBlocksCore.blockSmoothRedSandStone),
-	            "##",
-	            "##",
-	            '#', new ItemStack(BountifulBlocksCore.blockRedSandStone)
-	    );
-	    
-	    GameRegistry.addRecipe(new ItemStack(BountifulBlocksCore.blockChiseledRedSandStone,3),
-	            "###",
-	            "   ",
-	            "###",
-	            '#', new ItemStack(BountifulBlocksCore.blockRedSandStone)
-	   
-	    );
-	    
-	    
-	    GameRegistry.addRecipe(new ItemStack(Blocks.stonebrick,1,3),
-	            "#",
-	            "#",
-	            '#', new ItemStack(Blocks.stone_slab,1,5)
-	    );
-	    
-	    GameRegistry.addShapelessRecipe(new ItemStack(Blocks.mossy_cobblestone),
-	    		Blocks.cobblestone,
-	 	        new ItemStack(Blocks.vine)
-	 	);
-	    
-	    GameRegistry.addShapelessRecipe(new ItemStack(Blocks.stonebrick,1,1),
-	    		new ItemStack(Blocks.stonebrick,1,0),
-	 	        new ItemStack(Blocks.vine)
-	 	);
-	    
-
-	    
-	    GameRegistry.addRecipe(new ItemStack(BountifulBlocksCore.blockDiorite,1,13),
-	            "##",
-	            "##",
-	            '#', Blocks.end_stone
-	    );
-	    
-	    GameRegistry.addRecipe(new ItemStack(BountifulBlocksCore.blockDiorite,1,12),
-	            "#@",
-	            "@#",
-	            '#', Blocks.end_stone,
-	            '@', new ItemStack(Items.dye,1,5)
-	      );
-	    
-	    GameRegistry.addRecipe(new ItemStack(BountifulBlocksCore.blockPurpurPillar),
-	            "@",
-	            "@",
-	            '@', new ItemStack(BountifulBlocksCore.blockDiorite,1,12)
-	      );
-	    
-	   GameRegistry.addRecipe(new ItemStack(BountifulBlocksCore.blockFenceX,4,0),
-	            "   ",
-	    		"@@@",
-	            "@ @",
-	            '@', new ItemStack(Blocks.planks,1,4)
-	      );
-	      
-	    GameRegistry.addRecipe(new ItemStack(BountifulBlocksCore.blockFenceX,4,1),
-	            "   ",
-	    		"@@@",
-	            "@ @",
-	            '@', new ItemStack(Blocks.planks,1,5)
-	      );
-	    GameRegistry.addRecipe(new ItemStack(BountifulBlocksCore.blockFenceX,4,2),
-	    		"   ",
-	    		"@@@",
-	            "@ @",
-	            '@', new ItemStack(Blocks.planks,1,2)
-	      );
-	    GameRegistry.addRecipe(new ItemStack(BountifulBlocksCore.blockFenceX,4,3),
-	    		"   ",
-	    		"@@@",
-	            "@ @",
-	            '@', new ItemStack(Blocks.planks,1,1)
-	      );
-	    GameRegistry.addRecipe(new ItemStack(BountifulBlocksCore.blockFenceX,4,4),
-	    		"   ",
-	    		"@@@",
-	            "@ @",
-	            '@', new ItemStack(Blocks.planks,1,3)
-	      );
-	      
-	    GameRegistry.addShapelessRecipe(new ItemStack(BountifulBlocksCore.blockCoarseDirt,3,1),
-	    		new ItemStack(Blocks.dirt,9)
-	 	);
-	    
-	    GameRegistry.addRecipe(new ItemStack(BountifulBlocksCore.blockBarrier),
-	            "###",
-	            "# #",
-	            "###",
-	            '#', Blocks.glass
-	            );
-	    
-	    GameRegistry.addRecipe(new ItemStack(Items.leather),
-	            "##",
-	            "##",
-	            '#', BountifulBlocksCore.rabbitHide
-	    );
-	    
-		GameRegistry.addSmelting(new ItemStack(Blocks.stonebrick,1,0),new ItemStack(Blocks.stonebrick,1,2),0.1f);
-		GameRegistry.addSmelting(new ItemStack(BountifulBlocksCore.rawRabbit,1),new ItemStack(BountifulBlocksCore.cookedRabbit,1),0.35f);
-		 
-		     
-		
-
+	    if(!BountifulBlocksConfig.is1_8Mode){}
+	    if(!BountifulBlocksConfig.is1_9Mode){}
+	    if(!BountifulBlocksConfig.is1_10Mode){}
 		     }
 }
